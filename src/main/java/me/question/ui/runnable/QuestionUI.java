3f7053813b792a2e10d59736c921e30219265cbb
zenhao123 <zenhao5514@gmail.com>
2022-05-24 23:09:23 +0800

    ???

--- src/main/java/me/question/ui/runnable/QuestionUI.java
+++ src/main/java/me/question/ui/runnable/QuestionUI.java
@@ -13,10 +13,12 @@ public class QuestionUI {
     public String answer3;
     public String answer4;
     public String correctAns;
+    public int questionID;
 
     public void onAction() {
         switch (stage) {
             case 1 -> {
+                questionID = 100;
                 question = (questionCount + ": 1972年以后的计算机习惯上被称为第几代计算机");
                 answer1 = ("第三代计算机");
                 answer2 = ("第四代计算机");
@@ -26,6 +28,7 @@ public class QuestionUI {
                 correctAns = answer2;
             }
             case 2 -> {
+                questionID = 200;
                 question = (questionCount + ": 3G是在哪一年开始普遍使用的？");
                 answer1 = ("2002年");
                 answer2 = ("2003年");
@@ -35,6 +38,7 @@ public class QuestionUI {
                 correctAns = answer2;
             }
             case 3 -> {
+                questionID = 300;
                 question = (questionCount + ": 显卡的英文缩写是下列哪项？");
                 answer1 = ("GPU");
                 answer2 = ("CPU");
@@ -44,6 +48,7 @@ public class QuestionUI {
                 correctAns = answer1;
             }
             case 4 -> {
+                questionID = 400;
                 question = (questionCount + ": 电脑没有RAM会对电脑造成什么影响？");
                 answer1 = ("没有影响");
                 answer2 = ("电脑无法运行");
@@ -53,6 +58,7 @@ public class QuestionUI {
                 correctAns = answer2;
             }
             case 5 -> {
+                questionID = 500;
                 question = (questionCount + ": 第一个微处理器是由什么公司是生产？");
                 answer1 = ("Apple");
                 answer2 = ("AMD");
@@ -62,6 +68,7 @@ public class QuestionUI {
                 correctAns = answer3;
             }
             case 6 -> {
+                questionID = 600;
                 question = (questionCount + ": 以下哪项是线上广告和线下广告的共同点？");
                 answer1 = ("传播范围广");
                 answer2 = ("吸引用户");
@@ -71,6 +78,7 @@ public class QuestionUI {
                 correctAns = answer2;
             }
             case 7 -> {
+                questionID = 700;
                 question = (questionCount + ": 以下哪项不是假新闻的类型？");
                 answer1 = ("误导性内容");
                 answer2 = ("题文不符");
@@ -80,6 +88,7 @@ public class QuestionUI {
                 correctAns = answer3;
             }
             case 8 -> {
+                questionID = 800;
                 question = (questionCount + ": 以下哪项不是照片编辑软件？");
                 answer1 = ("GIMP");
                 answer2 = ("Adobe Photoshop");
@@ -89,6 +98,7 @@ public class QuestionUI {
                 correctAns = answer4;
             }
             case 9 -> {
+                questionID = 900;
                 question = (questionCount + ": 以下哪个是只能在手机正式使用的影片剪辑软件？");
                 answer1 = ("Kinemaster");
                 answer2 = ("剪映");
@@ -98,6 +108,7 @@ public class QuestionUI {
                 correctAns = answer1;
             }
             case 10 -> {
+                questionID = 1000;
                 question = (questionCount + ": 以下哪个不是社交媒体所利用的技术以便有效地发布广告及精准地推广用户热爱的资讯？");
                 answer1 = ("大数据");
                 answer2 = ("演算法");
@@ -107,6 +118,7 @@ public class QuestionUI {
                 correctAns = answer4;
             }
             case 11 -> {
+                questionID = 1100;
                 question = (questionCount + ": 以下哪项不是艾伦·图灵的誉称？");
                 answer1 = ("计算机科学之父");
                 answer2 = ("人工智能之父");
@@ -116,6 +128,7 @@ public class QuestionUI {
                 correctAns = answer4;
             }
             case 12 -> {
+                questionID = 1200;
                 question = (questionCount + ": PCIe和SATA哪个的带宽速度比较快?");
                 answer1 = ("PCIe");
                 answer2 = ("SATA");
@@ -125,6 +138,7 @@ public class QuestionUI {
                 correctAns = answer1;
             }
             case 13 -> {
+                questionID = 1300;
                 question = (questionCount + ": 以下哪项不是电子产品的品牌？");
                 answer1 = ("Apple");
                 answer2 = ("AMD");
@@ -134,6 +148,7 @@ public class QuestionUI {
                 correctAns = answer4;
             }
             case 14 -> {
+                questionID = 1400;
                 question = (questionCount + ": Windows XP的发行日是几时？");
                 answer1 = ("2001/10/25");
                 answer2 = ("2002/10/25");
@@ -143,6 +158,7 @@ public class QuestionUI {
                 correctAns = answer1;
             }
             case 15 -> {
+                questionID = 1500;
                 question = (questionCount + ": 第一台现代意义上的可以商用的移动电话在哪一年诞生？");
                 answer1 = ("1998");
                 answer2 = ("1987");
